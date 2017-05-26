a06dfa2553dffa5cc0eb2b710f6f9ea717eabb54
Michael Mai <michael.mai@brandingbrand.com>
2017-05-26 13:44:33 -0400

    Add regex to optimize

--- translate.js
+++ translate.js
@@ -1,5 +1,11 @@
 /*
+  Authors: Mike Mai & Ben Hanes
   CMD LINE: node translate.js [language to convert to]
+
+  Requirements:
+    npm install excel-as-json fs
+    .xlsx translations file
+    .json english translation file (en-US.json inside public/locales/translation/)
 */
 
 var fs            = require('fs');
@@ -7,7 +13,7 @@ var converter     = require('excel-as-json').processFile;
 var englishJSON   = './en-US.json';
 var excelSheet    = './excel.xlsx';
 var fromLanguage  = 'English';
-var toLanguage   = process.argv[2];
+var toLanguage    = process.argv[2];
 var languages     = [
   'Russian',
   'German',
@@ -30,7 +36,11 @@ converter(excelSheet, null, null,  function(err, data) {
       if(!element[fromLanguage]) return;
 
       try {
-        result = result.replace(element[fromLanguage], element[toLanguage]);
+        //Escape any characters with regex meanings
+        //Match any group of spaces to any other group of spaces
+        //Make all sentence ending punctuation at the end of a string optional
+        var regex = new RegExp(':\\s+"\\s*' + element[fromLanguage].trim().replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&').replace(/\s+/g, '\\s+').replace(/\\([\.\?\!])$/, '\\$1?') + '\\s*"', 'ig');
+        result = result.replace(regex, ': "' + element[toLanguage].trim() + '"');
       } catch(e) {};
     });
 
